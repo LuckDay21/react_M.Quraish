1f3fe919ccbed732276f42ab1faf1ac0b78fbe80
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-04-18 01:55:57 +0700

    miniProject - add components (NavbarComponent, ListCategories, Cart)

--- miniProject/dr.books/src/components/NavbarComponent.js
+++ miniProject/dr.books/src/components/NavbarComponent.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -9,9 +7,11 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 const NavbarComponent = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="#">
+            <strong>Dr. Books</strong>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -21,7 +21,7 @@ const NavbarComponent = () => {
             >
               <Nav.Link href="#action1">Home</Nav.Link>
               <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">
                   Another action
@@ -31,19 +31,7 @@ const NavbarComponent = () => {
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
             </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
