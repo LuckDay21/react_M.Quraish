908e01dc9b1b61b82f51292001483f59da72288f
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-04-17 21:40:45 +0700

    miniProject - instalasi ReactJS, react-bootstrap, sweetalert, axios

---
+++ miniProject/dr.books/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarComponent from "./components/NavbarComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <h2>Halo</h2>
+    </div>
+  );
+}
+
+export default App;
